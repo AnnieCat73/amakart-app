99b4bdb7ae433f1d7eb63510fd433ad5ac3778e6
Annie <devannie777@gmail.com>
2022-04-15 14:18:42 +0100

    ListItem component HTML set up

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,22 @@
 const ListItem = () => {
-  return <h2>List Item</h2>
+  return (
+    <div>
+      <img src="logo192.png" alt="Some title" />
+      <div>
+        <span>340</span>
+        <small>
+          <strike>450</strike>
+        </small>
+      </div>
+      <div>
+        <h3>Title of the Item</h3>
+      </div>
+      <button>
+        <span>Add to Card</span>
+        <img src=" " alt="" />
+      </button>
+    </div>
+  )
 }
 
 export default ListItem;
