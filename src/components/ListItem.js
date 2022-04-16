ad4941f043ac2e9fcea0c6c8f65e0f8f189235d5
Annie <devannie777@gmail.com>
2022-04-16 13:07:37 +0100

    Scss styles added to listItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,18 +2,18 @@ import AddToCartIcon from "../assets/icons/cart.png"
 
 const ListItem = () => {
   return (
-    <div>
-      <img src="logo192.png" alt="Some title" />
-      <div>
-        <span>340</span>
+    <div className={"item-card"}>
+      <img className={"img-fluid"}src="logo192.png" alt="Some title" />
+      <div className={"item-card__information"}>
+        <span className={"pricing"}>$340</span>
         <small>
-          <strike>450</strike>
+          <strike>$450</strike>
         </small>
       </div>
-      <div>
+      <div className={"title"}>
         <h3>Title of the Item</h3>
       </div>
-      <button>
+      <button className="cart-add">
         <span>Add to Card</span>
         <img style={{height: "1rem"}}src={AddToCartIcon} alt="Cart Icon" />
       </button>
