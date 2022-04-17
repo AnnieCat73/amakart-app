1d1190022fc12e521fd0e9df08b9c89108481388
Annie <devannie777@gmail.com>
2022-04-17 13:03:27 +0100

    Making ListItem more dynamic with passing data variable

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,23 @@
 import AddToCartIcon from "../assets/icons/add_cart.svg"
 
 const ListItem = () => {
+  const data = {
+    discountedPrice: 340,
+    price: 450,
+    title: "title of the Item",
+    thumbnail: "placeholder1.png"
+  }
   return (
     <div className={"item-card"}>
-      <img className={"img-fluid"}src="placeholder1.png" alt="Some title" />
+      <img className={"img-fluid"}src={`/${data.thumbnail}`} alt={data.title} />
       <div className={"item-card__information"}>
-        <span className={"pricing"}>$340</span>
+        <span className={"pricing"}>${data.discountedPrice}</span>
         <small>
-          <strike>$450</strike>
+          <strike>${data.price}</strike>
         </small>
       </div>
       <div className={"title"}>
-        <h3>Title of the Item</h3>
+        <h3>{data.title}</h3>
       </div>
       <button className="cart-add">
         <span>Add to Card</span>
