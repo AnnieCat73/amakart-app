72f2a0784b03cd00498d7b815c4606943918f0e6
Annie <devannie777@gmail.com>
2022-04-19 14:31:15 +0100

    ListItem inlne styling applied

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,15 +9,15 @@ const ListItem = ({ data }) => {
   } Use data as props in app.js <ListItem />*/
   return (
     <div className={"item-card"}>
-      <img className={"img-fluid"}src={`/${data.thumbnail}`} alt={data.title} />
+      <img className={"img-fluid"} src={`/${data.thumbnail}`} alt={data.title} />
       <div className={"item-card__information"}>
         <span className={"pricing"}>${data.discountedPrice}</span>
         <small>
-          <strike>${data.price}</strike>
+          <strike style={{ marginLeft: "8px" }}>${data.price}</strike>
         </small>
       </div>
       <div className={"title"}>
-        <h3>{data.title}</h3>
+        <h3 style={{ marginLeft: "8px" }}>{data.title}</h3>
       </div>
       <button className="cart-add">
         <span>Add to Card</span>
