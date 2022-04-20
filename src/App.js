8b2dcce4aea047de7c04036cae1bc8a43c3aff0c
Annie <devannie777@gmail.com>
2022-04-20 11:47:20 +0100

    Added practice file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import ListItem from "./components/ListItem";
+import Products from "./components/Products/Products";
 
 const App = () => {
   return (
     <div>
-     <ListItem data={{
-       title: "Title of this Item 1",
-       price: 450,
-       discountedPrice: 340,
-       thumbnail: "placeholder1.png"
-     }}/>
-     <ListItem data={{
-       title: "Title of this Item 2",
-       price: 100,
-       discountedPrice: 80,
-       thumbnail: "placeholder1.png"}}/>
-     
+      <Products />
+
     </div>
   );
 }
